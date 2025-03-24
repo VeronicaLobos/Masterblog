dbe518f77312a7f8386edc5662208a1902add0b9
VeronicaMG <33263432+VeronicaLobos@users.noreply.github.com>
2025-03-24 12:03:39 +0100

    Implement index.html and style.css, return render_template

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import webbrowser
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    ##  return 'Hello, World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
+    webbrowser.open_new('http://127.0.0.1:5001/')
     app.run(host="0.0.0.0", port=5001, debug=True)
