4b23d1a7d9f6e9adbc1da7c5309866b4f1c36574
VeronicaMG <33263432+VeronicaLobos@users.noreply.github.com>
2025-03-25 15:25:22 +0100

    Changed port to 5000

--- app.py
+++ app.py
@@ -201,5 +201,5 @@ if __name__ == '__main__':
     file_path = os.path.join('./data', 'blog_posts.json')
     database = HandleJson(file_path)
 
-    webbrowser.open_new('http://127.0.0.1:5001/')
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    webbrowser.open_new('http://127.0.0.1:5000/')
+    app.run(host="0.0.0.0", port=5000, debug=True)
